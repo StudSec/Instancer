817c5a96247acff6fcf4bfd9cf898775984b6b80
joachimBose <joachimbose48@gmail.com>
2025-03-22 18:31:15 +0100

    made: additional more useful test

--- test/test.py
+++ test/test.py
@@ -5,8 +5,10 @@ import fastapi.security
 import httpx
 import tomllib
 import asyncio
+import pytest
 from webapp.executor import Executor
 from webapp.config import Config
+from webapp.challenge import Challenge
 
 CONFIG_PATH = "config.toml"
 
@@ -24,7 +26,7 @@ def get_authentication():
 client = TestClient(app)
 config = Config(CONFIG_PATH)
 executor = Executor(config)
-
+pytest_plugins = ('pytest_asyncio',)
 
 asyncio.run(executor.create_enviroment())
 
@@ -33,8 +35,6 @@ app.extra = {
     "executor": executor
 }
 
-
-
 def test_start_stop_status_endpoints(): 
     user_id = "12345"  # Arbitrary user ID
     service_name = "buffer_overflow"
@@ -42,12 +42,30 @@ def test_start_stop_status_endpoints():
     auth = get_authentication()
     
     response = client.get(f"/start/{user_id}/{service_name}", auth=auth)
+    print(f"Instancer responded with {response.content}")
     assert response.status_code == 200
 
     response = client.get(f"/status/{user_id}/{service_name}", auth=auth)
+    print(f"Instancer responded to status with {response.content}")
     assert response.status_code == 200
     
     response = client.get(f"/stop/{user_id}/{service_name}", auth=auth)
+    print(f"Instancer responded to stop with {response.content}")
     assert response.status_code == 200
-    # Add specific assertions here as well
-    # assert response.json() == {"message": "Service stopped successfully"}
+
+@pytest.mark.asyncio
+async def test_start_instance():
+    user_id = "1234"
+    challenge_name = "buffer_overflow"
+    auth = get_authentication()
+
+    executor = app.extra["executor"]
+    challenge = app.extra["config"].challenges[challenge_name]
+    
+    if await challenge.working_set.contains_or_insert(user_id):
+        await challenge.start(executor, user_id)
+    
+    response = client.get(f"/status/{user_id}/{challenge_name}", auth=auth)
+    print(f"Instancer responded to status with {response.content}")
+    assert response.status_code == 200
+    assert response.content == b'{"state":"started"}'
